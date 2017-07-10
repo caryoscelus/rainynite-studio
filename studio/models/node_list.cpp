9e914cea1aac13d0ea6925d12308ea8abe416a12
caryoscelus <caryoscelus@gmx.com>
2017-07-11 01:09:43 +0300

    Node list model

---
+++ studio/models/node_list.cpp
@@ -0,0 +1,94 @@
+/*
+ *  node_list.cpp - node list model
+ *  Copyright (C) 2017 caryoscelus
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <core/node_info.h>
+
+#include <util/strings.h>
+#include "node_list.h"
+
+namespace studio {
+
+NodeListModel::NodeListModel(QObject* parent) :
+    QAbstractItemModel(parent)
+{
+}
+
+NodeListModel::~NodeListModel() {
+}
+
+QVariant NodeListModel::data(QModelIndex const& index, int role) const {
+    if (!index.isValid() || index.column() != 0 || index.parent().isValid())
+        return {};
+
+    switch (role) {
+        case Qt::DisplayRole: {
+            return util::str(core::node_name(*get_node(index)));
+        } break;
+        default:
+            return {};
+    }
+}
+
+Qt::ItemFlags NodeListModel::flags(QModelIndex const& /*index*/) const {
+    return Qt::ItemIsSelectable | Qt::ItemIsEnabled;
+}
+
+QVariant NodeListModel::headerData(int /*section*/, Qt::Orientation /*orientation*/, int /*role*/) const {
+    return {};
+}
+
+QModelIndex NodeListModel::index(int row, int column, QModelIndex const& parent) const {
+    if (parent.isValid() || column != 0)
+        return {};
+    return createIndex(row, column, nullptr);
+}
+
+QModelIndex NodeListModel::parent(QModelIndex const& /*index*/) const {
+    return {};
+}
+
+int NodeListModel::rowCount(QModelIndex const& parent) const {
+    if (parent.isValid())
+        return 0;
+    return nodes.size();
+}
+
+int NodeListModel::columnCount(QModelIndex const& /*parent*/) const {
+    return 1;
+}
+
+bool NodeListModel::removeRows(int row, int count, QModelIndex const& parent) {
+    if (parent.isValid())
+        return false;
+    nodes.erase(nodes.begin()+row, nodes.begin()+row+count);
+    return true;
+}
+
+void NodeListModel::insert_node(std::shared_ptr<core::AbstractValue> node, int position) {
+    if (position < 0)
+        position = nodes.size();
+    beginInsertRows({}, position, position+1);
+    nodes.insert(nodes.begin()+position, node);
+    endInsertRows();
+}
+
+std::shared_ptr<core::AbstractValue> NodeListModel::get_node(QModelIndex const& index) const {
+    return nodes[index.row()];
+}
+
+} // namespace studio
---
+++ studio/models/node_list.h
@@ -0,0 +1,58 @@
+/*
+ *  node_list.h - node list model
+ *  Copyright (C) 2017 caryoscelus
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __STUDIO__MODELS__NODE_LIST_H__CE0F8EE4
+#define __STUDIO__MODELS__NODE_LIST_H__CE0F8EE4
+
+#include <QAbstractItemModel>
+
+namespace core {
+class AbstractValue;
+}
+
+namespace studio {
+
+class NodeListModel : public QAbstractItemModel
+{
+public:
+    explicit NodeListModel(QObject* parent = nullptr);
+    virtual ~NodeListModel();
+
+public:
+    QVariant data(QModelIndex const& index, int role) const override;
+    Qt::ItemFlags flags(QModelIndex const& index) const override;
+    QVariant headerData(int section, Qt::Orientation orientation, int role=Qt::DisplayRole) const override;
+    QModelIndex index(int row, int column, QModelIndex const& parent={}) const override;
+    QModelIndex parent(QModelIndex const& index) const override;
+    int rowCount(QModelIndex const& parent={}) const override;
+    int columnCount(QModelIndex const& parent={}) const override;
+
+public:
+    bool removeRows(int row, int count, QModelIndex const& parent={}) override;
+    void insert_node(std::shared_ptr<core::AbstractValue> node, int position=-1);
+
+protected:
+    std::shared_ptr<core::AbstractValue> get_node(QModelIndex const& index) const;
+
+private:
+    std::vector<std::shared_ptr<core::AbstractValue>> nodes;
+};
+
+} // namespace studio
+
+#endif
