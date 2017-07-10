127d08d72da1a13b43dc206bfb53a4405dc21e11
caryoscelus <caryoscelus@gmx.com>
2017-07-10 18:07:31 +0300

    TimePeriod editor

---
+++ studio/canvas/time_period_editor.cpp
@@ -0,0 +1,98 @@
+/*
+ *  time_period_editor.cpp - time period editing widget
+ *  Copyright (C) 2017 caryoscelus
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <QGraphicsRectItem>
+
+#include <generic/node_editor.h>
+#include <generic/timeline_editor.h>
+#include <widgets/timeline_area.h>
+#include "time_item.h"
+
+namespace studio {
+
+class TimePeriodEditor :
+    public TimelineEditor,
+    public NodeEditor
+{
+public:
+    void set_canvas(TimelineArea* canvas) override {
+        TimelineEditor::set_canvas(canvas);
+        /**
+         * TODO: generalize and move out
+         */
+        auto item_lambda = [this](auto set_node_lambda) {
+            return [this, set_node_lambda](core::Time time) {
+                if (auto node = get_node_as<core::TimePeriod>()) {
+                    ignore_time_change = true;
+                    if (node->can_set()) {
+                        set_node_lambda(node, time);
+                        node->changed();
+                    }
+                    ignore_time_change = false;
+                }
+            };
+        };
+        first_item = std::make_unique<TimeItem>(
+            item_lambda([](auto node, auto time) {
+                node->mod().set_first(time);
+            })
+        );
+        last_item = std::make_unique<TimeItem>(
+            item_lambda([](auto node, auto time) {
+                node->mod().set_last(time);
+            })
+        );
+        canvas->scene()->addItem(first_item.get());
+        canvas->scene()->addItem(last_item.get());
+        node_update();
+    }
+public:
+    void node_update() override {
+        update_position();
+        auto node = get_node_as<core::TimePeriod>();
+        bool editable = node && node->can_set();
+        for (auto item : {first_item.get(), last_item.get()}) {
+            item->set_readonly(!editable);
+            item->setFlag(QGraphicsItem::ItemIsSelectable, editable);
+        }
+    }
+    void time_changed(core::Time time) override {
+        ContextListener::time_changed(time);
+        update_position();
+    }
+private:
+    void update_position() {
+        if (first_item && !ignore_time_change) {
+            if (auto maybe_value = get_value<core::TimePeriod>()) {
+                auto value = *maybe_value;
+                first_item->set_fps(value.get_fps());
+                first_item->move_to(value.get_first());
+                last_item->set_fps(value.get_fps());
+                last_item->move_to(value.get_last());
+            }
+        }
+    }
+private:
+    std::unique_ptr<TimeItem> first_item;
+    std::unique_ptr<TimeItem> last_item;
+    bool ignore_time_change = false;
+};
+
+REGISTER_TIMELINE_NODE_EDITOR(TimePeriodEditor, core::TimePeriod, TimePeriodEditor);
+
+}
