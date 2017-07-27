c1a632de3c26b5aab7f84034dbaa83abf6646ec5
caryoscelus <caryoscelus@gmx.com>
2017-07-27 21:52:26 +0300

    Pen helper

---
+++ studio/util/pen.h
@@ -0,0 +1,34 @@
+/*
+ *  pen.h - QPen utils
+ *  Copyright (C) 2017 caryoscelus
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __STUDIO__UTIL__PEN_H__51304424
+#define __STUDIO__UTIL__PEN_H__51304424
+
+#include <QPen>
+#include <QGuiApplication>
+#include <QPalette>
+
+namespace studio {
+namespace pens {
+
+QPen cosmetic_dash();
+
+} // namespace pens
+} // namespace studio
+
+#endif
