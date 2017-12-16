f0a05117d909b38a48730c2301916c90f6a12918
caryoscelus <caryoscelus@gmx.com>
2017-12-16 04:33:53 +0300

    Update to core

--- studio/models/node_model.cpp
+++ studio/models/node_model.cpp
@@ -72,7 +72,7 @@ QVariant NodeModel::data(QModelIndex const& index, int role) const {
             } break;
             case Qt::ToolTipRole: {
                 // TODO: node role documentation
-                if (auto node = get_node_as<core::DocString>(index))
+                if (auto node = get_node_as<DocString>(index))
                     return util::str(node->doc_string());
                 return {};
             } break;
