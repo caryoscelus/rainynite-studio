32f5760397bd15cadd8855f97c1b9e73e4b09f49
caryoscelus <caryoscelus@gmx.com>
2017-12-22 14:40:54 +0300

    lib2geom <-> Qt affine type conversions

--- studio/util/geom.h
+++ studio/util/geom.h
@@ -26,6 +26,13 @@
 
 namespace rainynite::util {
 
+
+/**
+ * Convert 2D point between lib2geom and Qt (QPoint/QPointF).
+ *
+ * This template function can be called whenever you need the conversion and
+ * direction will be detected automatically throw template specializations.
+ */
 template <typename S, typename T>
 T point(S&& p);
 
@@ -46,6 +53,32 @@ std::enable_if_t<is_same_v<std::decay_t<S>,QPoint>, Geom::Point>
 point(S&& p) {
     return { (Geom::Coord) p.x(), (Geom::Coord) p.y() };
 }
+
+
+/**
+ * Convert affine transform matrices between lib2geom (Affine) and Qt (QMatrix).
+ *
+ * Template specializations will automatically choose appropriate instance.
+ *
+ * NOTE: since QTransform is full 3x3 matrix, it cannot be converted into
+ * Geom::Affine without possible information loss, so you have to do that
+ * explicitly.
+ */
+template <typename S, typename T>
+T matrix(S&& m);
+
+template <typename S>
+std::enable_if_t<is_same_v<std::decay_t<S>,Geom::Affine>, QMatrix>
+matrix(S&& m) {
+    return {m[0], m[1], m[2], m[3], m[4], m[5]};
+}
+
+template <typename S>
+std::enable_if_t<is_same_v<std::decay_t<S>,QMatrix>, Geom::Affine>
+matrix(S&& m) {
+    return {m.m11(), m.m12(), m.m21(), m.m22(), m.dx(), m.dy()};
+}
+
 
 } // namespace rainynite::util
 
