11d703e5cbc4d273c4ea7426867b7cca3ebc6bf2
caryoscelus <caryoscelus@gmx.com>
2017-12-23 21:10:13 +0300

    Separate part of import actions code into header

--- studio/actions/import.cpp
+++ studio/actions/import.cpp
@@ -15,16 +15,12 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include <QFileDialog>
-
-#include <core/document.h>
 #include <core/node_info.h>
 #include <core/action_stack.h>
 #include <core/actions/list.h>
 
-#include <generic/action.h>
-#include <util/strings.h>
 #include "process_node.h"
+#include "import.h"
 
 namespace rainynite::studio::actions {
 
@@ -108,77 +104,6 @@ public:
 };
 
 REGISTER_PROCESS_NODE(FillRenderableListWithSvgs, string, ImportSvgLayersTag)
-
-template <typename Processor>
-class FindTargetNode {
-public:
-    unique_ptr<Processor> find_appropriate_target(shared_ptr<EditorContext> ctx) {
-        if (auto active_node = ctx->get_active_node()) {
-            if (auto r = check_target(active_node, ctx))
-                return r;
-            if (auto list = list_cast(active_node)) {
-                for (auto child : list->get_links()) {
-                    if (auto r = check_target(child, ctx))
-                        return r;
-                }
-            }
-        }
-        if (auto doc = ctx->get_context()->get_document()) {
-            if (auto root = list_cast(doc->get_property("root"))) {
-                for (auto child : root->get_links()) {
-                    if (auto r = check_target(child, ctx))
-                        return r;
-                }
-            }
-        }
-        return {};
-    }
-
-    unique_ptr<Processor> check_target(shared_ptr<core::AbstractValue> node, shared_ptr<EditorContext> ctx) const {
-        using namespace class_init;
-        for (auto factory : class_list_registry<AbstractFactory<Processor>>()) {
-            auto r = (*factory)();
-            r->set_context(ctx);
-            if (r->set_node(node))
-                return r;
-        }
-        return nullptr;
-    }
-};
-
-struct ImportError : public std::runtime_error {
-    ImportError(string const& msg) :
-        std::runtime_error(msg)
-    {}
-};
-
-/**
- * Import file paths
- */
-template <typename Processor>
-class ImportFiles :
-    public UiAction,
-    public ContextListener,
-    FindTargetNode<Processor>
-{
-public:
-    void process() override {
-        if (auto ctx = get_context()) {
-            if (auto target = this->find_appropriate_target(ctx)) {
-                // TODO: filter images
-                auto strings = QFileDialog::getOpenFileNames(nullptr, "Import layers");
-                target->start_list();
-                for (auto const& s : strings)
-                    target->feed(util::str(s));
-                target->end_list();
-            } else {
-                throw ImportError("Cannot find suitable target node.");
-            }
-        } else {
-            throw ImportError("No context! This is a bug.");
-        }
-    }
-};
 
 /**
  * Import file paths as Image layers
