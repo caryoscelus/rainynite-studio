2dc0ebcdbde7f514377f8331d368f635dd0e76b4
caryoscelus <caryoscelus@gmx.com>
2017-12-24 16:05:07 +0300

    Bezier tool: allow replacing

--- studio/canvas/tools/bezier.cpp
+++ studio/canvas/tools/bezier.cpp
@@ -21,6 +21,9 @@
 #include <core/node/node.h>
 #include <core/node/make.h>
 #include <core/node_info.h>
+#include <core/action_stack.h>
+#include <core/actions/change_value.h>
+#include <core/exceptions.h>
 
 #include <geom_helpers/knots.h>
 
@@ -53,7 +56,12 @@ protected:
     bool mouse_press(QPoint const& pos) override {
         if (!is_drawing) {
             is_drawing = true;
-            path_node = make_shared<core::Value<Geom::BezierKnots>>();
+            if (inplace) {
+                old_path = path_node->mod();
+                path_node->set({});
+            } else {
+                path_node = make_shared<core::Value<Geom::BezierKnots>>();
+            }
             path.reset(&path_node->mod());
             path->closed = false;
             editor = make_unique<BezierEditor>();
@@ -101,6 +109,33 @@ protected:
         return false;
     }
 
+    bool accept(shared_ptr<core::AbstractValue> node) override {
+        // TODO: universal fix
+        if (Shape::accept(node))
+            return true;
+        if (auto bezier_node = dynamic_pointer_cast<core::Value<Geom::BezierKnots>>(std::move(node))) {
+            path_node = std::move(bezier_node);
+            inplace = true;
+            return true;
+        }
+        return false;
+    }
+
+    void write_shape(shared_ptr<core::AbstractValue> shape) override {
+        if (inplace) {
+            auto action_stack = get_canvas()->get_context()->action_stack();
+            if (action_stack == nullptr)
+                throw NullPointerException("Cannot write to empty action stack.");
+            // restore old value for undo
+            auto new_path = path_node->mod();
+            path_node->set(old_path);
+            action_stack->emplace<core::actions::ChangeValue>(path_node, new_path);
+            action_stack->close();
+        } else {
+            Shape::write_shape(shape);
+        }
+    }
+
 private:
     Geom::Point convert_pos(QPoint const& pos) {
         return util::point(get_canvas()->mapToScene(pos));
@@ -111,6 +146,8 @@ private:
     bool is_pressed = false;
     unique_ptr<BezierEditor> editor;
     shared_ptr<core::Value<Geom::BezierKnots>> path_node;
+    Geom::BezierKnots old_path;
+    bool inplace = false;
     observer_ptr<Geom::BezierKnots> path;
     Geom::Point next_pos;
 };
