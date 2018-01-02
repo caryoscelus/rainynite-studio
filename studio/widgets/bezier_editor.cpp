cfbadf10601c39973e752a517ddc75a986eb0336
caryoscelus <caryoscelus@gmx.com>
2018-01-03 02:55:41 +0300

    Bezier editor widget: close path

---
+++ studio/widgets/bezier_editor.cpp
@@ -0,0 +1,57 @@
+/*  bezier_editor.cpp - bezier editor helper widget
+ *  Copyright (C) 2018 caryoscelus
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <core/action_stack.h>
+
+#include <generic/node_editor.h>
+#include <generic/custom_widgets.h>
+#include "ui_bezier_editor.h"
+
+namespace rainynite::studio {
+
+class BezierEditorWidget : public QWidget, public NodeEditor {
+public:
+    BezierEditorWidget() :
+        ui(make_unique<Ui::BezierEditorWidget>())
+    {
+        ui->setupUi(this);
+        connect(ui->is_closed, &QCheckBox::toggled, this, &BezierEditorWidget::set_closed);
+    }
+    virtual ~BezierEditorWidget() {
+    }
+
+protected:
+    void set_closed(bool closed) {
+        if (auto action_stack = get_context()->action_stack()) {
+            if (auto node = get_node_as<Geom::BezierKnots>()) {
+                if (node->is_const()) {
+                    auto value = node->mod();
+                    value.closed = closed;
+                    action_stack->emplace<core::actions::ChangeValue>(node, value);
+                    action_stack->close();
+                }
+            }
+        }
+    }
+
+private:
+    unique_ptr<Ui::BezierEditorWidget> ui;
+};
+
+REGISTER_CUSTOM_WIDGET(BezierEdit, Geom::BezierKnots, BezierEditorWidget);
+
+} // namespace rainynite::studio
